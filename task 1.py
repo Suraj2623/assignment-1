46f9928736d2df3491897e5ea22c73f1912bad0f
Suraj tatkare <suraj.tatkare23@gmail.com>
2025-04-22 23:56:57 +0530

    Initial commit

---
+++ task 1.py
@@ -0,0 +1,12 @@
+a=input("enter then first number:")
+b=input("enter then second number:")
+a=float(a)
+b=float(b)
+c=a+b
+print("addition: ",c)
+d=a-b
+print("subraction: ",d)
+e=a*b
+print("multiplication: ",e)
+f=a/b
+print("division: ",f)
